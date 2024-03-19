4fe1405e892b7e7a4b027946bc127e85e533289a
alexisquintanilla <quintanilla.alexis@gmail.com>
2024-03-18 23:12:39 -0300

    resolver herencia

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ const multimediaDom = (() => {
     }
     return {
         // clausula
-        publico(id, url) {
+        insertarUrl(id, url) {
             multimediaAtributo(id, "src", url)
         }
     }
@@ -18,47 +18,48 @@ const peliculas = document.querySelector("#peliculas")
 const series = document.querySelector("#series")
 
 // CREACION DE PLANTILLAS
-class Reproductor {
-    #id
-    constructor(id) {
-        this.#id = id
-    }
-    playMultimedia() {
-        multimediaDom.publico(this.#id, this.getUrl)
-    }
-    setInicio(tiempo) {
-        const modificarTiempo = `${this.getUrl}?start=${tiempo}`
-        multimediaDom.publico(this.#id, modificarTiempo)
-        console.log(modificarTiempo)
-    }
-
-}
-
-class Multimedia extends Reproductor {
+class Multimedia {
     #url
-    constructor(id, url) {
-        super(id)
+    constructor(url) {
         this.#url = url
     }
     get getUrl() {
         return this.#url
     }
-    // setInicio() {
-    //     return "Este método es para realizar un cambio en la URL del video"
-    // }
+    setInicio() {
+        return "Este método es para realizar un cambio en la URL del video"
+    }
 }
+
+class Reproductor extends Multimedia {
+    #id
+    constructor(id, url) {
+        super(url)
+        this.#id = id
+    }
+    playMultimedia() {
+        multimediaDom.insertarUrl(this.#id, this.getUrl)
+    }
+    setInicio(tiempo) {
+        const modificarTiempo = `${this.getUrl}?start=${tiempo}`
+        multimediaDom.insertarUrl(this.#id, modificarTiempo)
+        console.log(modificarTiempo)
+    }
+
+}
+
 
 // CREACION DE INSTANCIAS Y EJECUCION DE FUNCIONES.
 
-const reproMusica = new Multimedia(musica, "https://www.youtube.com/embed/CD-E-LDc384")
+const reproMusica = new Reproductor(musica, "https://www.youtube.com/embed/CD-E-LDc384")
 reproMusica.playMultimedia()
 reproMusica.setInicio(30)
 
-const reproPelicula = new Multimedia(peliculas, "https://www.youtube.com/embed/1roy4o4tqQM?si=Psyd5qnWDMGCvwpd")
+const reproPelicula = new Reproductor(peliculas, "https://www.youtube.com/embed/1roy4o4tqQM?si=Psyd5qnWDMGCvwpd")
 reproPelicula.playMultimedia()
 reproPelicula.setInicio(33)
 
-const reproSerie = new Multimedia(series, "https://www.youtube.com/embed/a1zmhHLVrq0?si=iuky9OuRIVGZ-BCA")
+const reproSerie = new Reproductor(series, "https://www.youtube.com/embed/a1zmhHLVrq0?si=iuky9OuRIVGZ-BCA")
 
 reproSerie.playMultimedia()
 reproSerie.setInicio(35)
